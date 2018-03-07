61b7af19981b7bf5d88cd6bdfd7e370bee387a17
sheesa azeeleeza <leezaluncheon@gmail.com>
2018-03-07 00:01:35 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(cell)
-  puts "#{cell[0]}|#{cell[1]}|#{cell[2]}"
+  puts " #{cell[0]} | #{cell[1]} | #{cell[2] }"
   puts "-----------"
-  puts "#{cell[3]}|#{cell[4]}|#{cell[5]}"
+  puts " #{cell[3]} | #{cell[4]} | #{cell[5] }"
   puts "-----------"
-  puts "#{cell[4]}|#{cell[5]}|#{cell[8]}"
+  puts " #{cell[4]} | #{cell[5]} | #{cell[8] }"
 end
